afe7a137874c249097868eab2d828a7cad5478d8
Gregory Dickison <gregorydickison@Gregorys-Air.frontierlocal.net>
2020-05-03 20:04:54 -0700

    A little cleanup

--- src/placeholders.js
+++
@@ -1,4 +0,0 @@
-/* const googleChartGoesHere = document.getElementById("google-chart");
-const googleChartPlaceholder = document.createElement("h2");
-googleChartPlaceholder.innerHTML = ("Google chart goes here");
-googleChartGoesHere.appendChild(googleChartPlaceholder); */
